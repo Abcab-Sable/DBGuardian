87225fe425ab9a19271caebbe31fdc709bfd5edc
Gabriel <272199@student.calderdale.ac.uk>
2025-05-25 19:54:34 +0100

    add measures to avoid using relative paths
    finish test_inc_db_path test
    
    add test for non-existent db_path
    
    add test for successful execution

--- python_code/connecttest.py
+++ python_code/connecttest.py
@@ -4,10 +4,14 @@ import pathlib
 import os
 import logging
 
+SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
+
+
 def release_logging_handlers():
     for handler in logging.root.handlers[:]:
         logging.root.removeHandler(handler)
         handler.close()
+
 
 class TestLogPath(unittest.TestCase):
 
@@ -15,7 +19,9 @@ class TestLogPath(unittest.TestCase):
         release_logging_handlers()
         error = False
         try:
-            con = connect.connect_sqlite(cor_db_path, inc_log_path)
+            con = connect.connect_sqlite(
+                str(SCRIPT_DIR / cor_db_path), str(SCRIPT_DIR / inc_log_path)
+            )
             con.close()
 
         except (FileNotFoundError, OSError, ValueError):
@@ -27,13 +33,15 @@ class TestLogPath(unittest.TestCase):
         release_logging_handlers()
         error = False
         success = False
-        log_path = pathlib.Path(cor_log_path)
+        log_path = SCRIPT_DIR / pathlib.Path(cor_log_path)
 
         if log_path.exists():
             os.remove(log_path)
-            
+
         try:
-            con = connect.connect_sqlite(cor_db_path, cor_log_path)
+            con = connect.connect_sqlite(
+                str(SCRIPT_DIR / cor_db_path), str(SCRIPT_DIR / cor_log_path)
+            )
             con.close()
 
         except:
@@ -51,14 +59,85 @@ class TestDBPath(unittest.TestCase):
     def test_inc_db_path(self, cor_log_path="./test.log", inc_db_path="./db./db"):
         release_logging_handlers()
         error = False
+        log = []
+        expected_log = [
+            "INFO:connect:STARTED\n",
+            f"INFO:connect:log_path: {str(SCRIPT_DIR / cor_log_path)}\n",
+            "ERROR:connect:FileNotFoundError\n",
+            "INFO:connect:FINISHED\n",
+        ]
 
         try:
-            con = connect.connect_sqlite(inc_db_path, cor_log_path)
+            con = connect.connect_sqlite(
+                str(SCRIPT_DIR / inc_db_path), str(SCRIPT_DIR / cor_log_path)
+            )
             con.close()
         except FileNotFoundError:
             error = True
 
+        with open(str(SCRIPT_DIR / cor_log_path), "r") as f:
+            log = f.readlines()
+
+        self.assertEqual(
+            log,
+            expected_log,
+            "Log should start, log_path should be logged, FileNotFoundError should be logged, Log should finish",
+        )
         self.assertEqual(error, True, "A FileNotFoundError should occur")
+
+    def test_nexist_db_path(
+        self, cor_log_path="./test.log", nexist_db_path="./nexist.db"
+    ):
+        release_logging_handlers()
+        error = False
+        log = []
+        expected_log = [
+            "INFO:connect:STARTED\n",
+            f"INFO:connect:log_path: {str(SCRIPT_DIR / cor_log_path)}\n",
+            "ERROR:connect:FileNotFoundError\n",
+            "INFO:connect:FINISHED\n",
+        ]
+
+        try:
+            con = connect.connect_sqlite(
+                str(SCRIPT_DIR / nexist_db_path), str(SCRIPT_DIR / cor_log_path)
+            )
+            con.close()
+        except FileNotFoundError:
+            error = True
+
+        with open(str(SCRIPT_DIR / cor_log_path), "r") as f:
+            log = f.readlines()
+
+        self.assertEqual(
+            log,
+            expected_log,
+            "Log should start, log_path should be logged, FileNotFoundError should be logged, Log should finish",
+        )
+        self.assertEqual(error, True, "A FileNotFoundError should occur")
+
+
+class TestSuccess(unittest.TestCase):
+
+    def test_success(self, cor_db_path="../db.db", cor_log_path="./test.log"):
+        log = []
+        expected_log = [
+            "INFO:connect:STARTED\n",
+            f"INFO:connect:log_path: {str(SCRIPT_DIR / cor_log_path)}\n",
+            f"INFO:connect:db_path: {str(SCRIPT_DIR / cor_db_path)}\n",
+            f"INFO:connect:Successfully connected with {pathlib.Path(str(SCRIPT_DIR / cor_db_path)).name}\n",
+            "INFO:connect:FINISHED\n"
+        ]
+
+        con = connect.connect_sqlite(
+            str(SCRIPT_DIR / cor_db_path), str(SCRIPT_DIR / cor_log_path)
+        )
+
+        with open(str(SCRIPT_DIR / cor_log_path), "r") as f:
+            log = f.readlines()
+
+        self.assertEqual(log, expected_log, "Log Should: log start, log log_path, log db_path, log success, log finish")
+        self.assertEqual(type(con).__name__, "Connection", "connect_sqlite should return a valid sqlite3 Connection object")
 
 if __name__ == "__main__":
     unittest.main()
