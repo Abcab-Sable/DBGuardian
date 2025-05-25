d1418cf33b31d5ae3b9f68278bf7180a1d261046
Gabriel <272199@student.calderdale.ac.uk>
2025-05-25 22:17:55 +0100

    use dotenv in connect.py to load log file path
    delete validation for log_path, by attempting to catch error using the open function, because logging.basicConfig does it anyway.
    
    add current working directory to log at start, to make relative paths clearer.

--- python_code/connect.py
+++ python_code/connect.py
@@ -1,11 +1,14 @@
 # This is the python file, which sets up database connections
 
+from dotenv import load_dotenv
 import sqlite3 # Usage of the sqlite3 library is required in order to connect to sqlite databases
 import pathlib # pathlib is required in order to check if files and directories exist
 import logging # logging is used to log important steps in the code execution, to provide a comprehensive review if something goes wrong
-import os # os is used to remove files
+import os
 
 logger = logging.getLogger(__name__) # logger object is used to actually log the steps
+load_dotenv()
+LOG_PATH = os.getenv("CONNECTPY_LOG_FILE_PATH")
 
 # This function takes parameters: db_path and log_path, which represent the database file path and the logging file path.
 # The database file is required to exist beforehand, the logging file is not.
@@ -14,12 +17,11 @@ logger = logging.getLogger(__name__) # logger object is used to actually log the
 # Third it validates that the db_path exists, which also simulataneously checks that it is valid.
 # Fourth it logs the db_path and attempts to establish a connection with the database.
 # Fifth it either logs a success and finish and returns the connection or it fails, logs the specific error and logs finish.
-def connect_sqlite(db_path, log_path="./connectpy.log"):
-    with open(log_path, "w"): # this will raise an error if the log_path is invalid, which is picked up on by unit tests.
-        pass
-
+def connect_sqlite(db_path, log_path=LOG_PATH):
+    #print(f"--- db_path: {db_path} ---")
     logging.basicConfig(filename=log_path, level=logging.INFO) # this creates a basic logging configuration
-    logger.info("STARTED")
+    cwd = os.getcwd()
+    logger.info(f"STARTED AT CWD: {cwd}")
     logger.info(f"log_path: {log_path}")
     try:
         db_path = pathlib.Path(db_path) # this converts db_path to a pathlib.Path object.
@@ -38,4 +40,5 @@ def connect_sqlite(db_path, log_path="./connectpy.log"):
 
 
 if __name__ == "__main__":
-    connect_sqlite("../db./db", "./logging.logging") # simple manual test to see how the program handles invalid database paths
+    #connect_sqlite("./db.db") # simple manual test to see how the program handles invalid database paths
+    pass
