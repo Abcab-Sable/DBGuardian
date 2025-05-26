01b6bf62c95abebda7427b80731ed995f238fb1a
Gabriel <272199@student.calderdale.ac.uk>
2025-05-26 20:23:52 +0100

    modularise the connect module, to adhere to the single responsibility rule
    Increase specificity of error handling.
    
    log full stack trace of exception, not just the type

--- python_code/connect.py
+++ python_code/connect.py
@@ -9,6 +9,17 @@ import os
 logger = logging.getLogger(__name__) # logger object is used to actually log the steps
 load_dotenv()
 LOG_PATH = os.getenv("CONNECTPY_LOG_FILE_PATH")
+CWD = os.getcwd()
+
+def validate_path_existence(path):
+    path = pathlib.Path(path)
+    if not path.exists():
+        raise FileNotFoundError
+    
+    return path
+
+def setup_logger(log_path=LOG_PATH):
+    logging.basicConfig(filename=log_path, level=logging.INFO)
 
 # This function takes parameters: db_path and log_path, which represent the database file path and the logging file path.
 # The database file is required to exist beforehand, the logging file is not.
@@ -18,23 +29,21 @@ LOG_PATH = os.getenv("CONNECTPY_LOG_FILE_PATH")
 # Fourth it logs the db_path and attempts to establish a connection with the database.
 # Fifth it either logs a success and finish and returns the connection or it fails, logs the specific error and logs finish.
 def connect_sqlite(db_path, log_path=LOG_PATH):
-    #print(f"--- db_path: {db_path} ---")
-    logging.basicConfig(filename=log_path, level=logging.INFO) # this creates a basic logging configuration
-    cwd = os.getcwd()
-    logger.info(f"STARTED AT CWD: {cwd}")
+
+    setup_logger(log_path)
+    logger.info(f"STARTED AT CWD: {CWD}")
     logger.info(f"log_path: {log_path}")
+
     try:
-        db_path = pathlib.Path(db_path) # this converts db_path to a pathlib.Path object.
-        if not db_path.exists():
-            raise FileNotFoundError # a FileNotFoundError is raised if the db_path does not exist.
+        db_path = validate_path_existence(db_path)
         logger.info(f"db_path: {db_path}")
         con = sqlite3.connect(db_path) # an attempt to connect to the database is made
         logger.info(f"Successfully connected with {db_path.name}")
         logger.info("FINISHED")
         return con # if successful, the connection to the database is returned
 
-    except Exception as e:
-        logger.error(f"{type(e).__name__}") # if an error occurs, it is logged
+    except (FileNotFoundError, PermissionError, OSError, sqlite3.OperationalError, sqlite3.DatabaseError, IOError, ValueError, sqlite3.Error) as e:
+        logger.exception(e) # if an error occurs, it is logged
         logger.info("FINISHED")
         raise e
 
